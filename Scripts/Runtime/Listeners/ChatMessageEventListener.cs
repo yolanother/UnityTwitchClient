ee26a66eb019279865e1a14f550f93626ba4177a
Yolan <yolanother@gmail.com>
2021-10-09 10:40:07 -0700

    Update dependencies

--- Scripts/Runtime/Listeners/ChatMessageEventListener.cs
+++ Scripts/Runtime/Listeners/ChatMessageEventListener.cs
@@ -10,13 +10,29 @@ namespace DoubTech.TwitchClient.Listeners
 {
     [Serializable]
     public class
-        ChatMessageEventListener : GameEventListener<ChatMessage, ChatMessageEvent, ChatMessageUnityEvent>
+        ChatMessageEventListener : GameEventListener<ChatMessage, ChatMessageEvent,
+            ChatMessageUnityEvent>
     {
+        [SerializeField] public string senderFilter;
+
         [SerializeField] private ChatMessageEvent gameEvent;
         [SerializeField] private ChatMessageUnityEvent onEvent = new ChatMessageUnityEvent();
 
         public override ChatMessageEvent GameEvent => gameEvent;
         public override ChatMessageUnityEvent OnEvent => onEvent;
+
+        public override void Invoke(ChatMessage t)
+        {
+            base.Invoke(t);
+        }
+
+        public override void OnEventRaised(ChatMessage t)
+        {
+            if (string.IsNullOrEmpty(senderFilter) || t.UserId == senderFilter)
+            {
+                base.OnEventRaised(t);
+            }
+        }
     }
 
     [Serializable]
--- Scripts/Runtime/UserInfo.cs
+++ Scripts/Runtime/UserInfo.cs
@@ -1,4 +1,5 @@
 using System;
+using TwitchLib.Client.Models;
 
 namespace DoubTech.TwitchClient
 {
@@ -8,5 +9,30 @@ namespace DoubTech.TwitchClient
         public string username;
         public string displayName;
         public string userId;
+
+        public UserInfo() {}
+        public UserInfo(ChatMessage message) => new UserInfo()
+        {
+            username = message.Username,
+            userId = message.UserId,
+            displayName = message.DisplayName
+        };
+
+        public UserInfo(ChatCommand command) => new UserInfo()
+        {
+            username = command.ChatMessage.Username,
+            userId = command.ChatMessage.UserId,
+            displayName = command.ChatMessage.DisplayName
+        };
+
+        public static explicit operator UserInfo(ChatMessage message) => new UserInfo(message);
+
+        public static explicit operator UserInfo(ChatCommand command) => new UserInfo(command);
+    }
+
+    public static class UserInfoUtil
+    {
+        public static UserInfo UserInfo(this ChatMessage message) => new UserInfo(message);
+        public static UserInfo UserInfo(this ChatCommand command) => new UserInfo(command);
     }
 }
